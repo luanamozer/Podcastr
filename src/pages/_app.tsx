7a1af344ec1b29ec5951c362ccfee25f6bb0bc8b
Luana Mozer <luanamozzer1996@gmail.com>
2021-04-20 15:55:35 -0300

    configurando json-server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import { Header } from '../components/Header'
-import '../styles/global.scss'
+import '../styles/global.scss';
+
+import styles from '../styles/app.module.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header/>
-      <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
     </div>
   )
 }
