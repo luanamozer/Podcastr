03ff090710b11b6a6f1702e5cd80621de6e028f5
Luana Mozer <luanamozzer1996@gmail.com>
2021-04-21 18:42:40 -0300

    alterando fetch por axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+    id: string;
+    title: string;
+    members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
@@ -7,9 +20,15 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes',{
+    params:{
+      _limit:12,
+      _sort: 'pulished_at',
+      _order: 'desc'
+    }
+  });
+  
 
   return {
     props: {
